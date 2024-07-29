b26bae964d6eddc8bc4c0106cb3b818d58645b3e
aref <m.h.aref2002>
2024-07-29 11:16:54 +0330

    add new mapping

--- lua/aref/core/keymaps.lua
+++ lua/aref/core/keymaps.lua
@@ -30,3 +30,7 @@ keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" 
 keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
 keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- terminal split
+keymap.set("n", "<leader>th", ":split | terminal<CR> | i", { noremap = true, silent = true })
+keymap.set("n", "<leader>tv", ":vsplit | terminal<CR> | i", { noremap = true, silent = true })
